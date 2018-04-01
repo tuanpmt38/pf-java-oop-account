9d61e77d4c7bfdc1840a8e3e03e3f461a35d84a8
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-01 16:30:35 +0700

    pf java oop account

--- Account.java
+++ Account.java
@@ -1,4 +1,64 @@
+import java.util.Date;
+
 public class Account {
+    private int id = 0;
+    private double balance = 0;
+    private double annualInterestRate = 0;
+    private Date dateCreated;
+
     public Account() {
     }
+
+    public Account(int id, double balance, double annualInterestRate) {
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+    public double getMonthlyInterestRate(){
+        double monthlyInterestRate = (annualInterestRate/100) / 12;
+        return monthlyInterestRate;
+    }
+    public double  getMonthlyInterest(){
+        return getMonthlyInterestRate()*balance;
+    }
+    public double withDraw(double withdrawalMoney) {
+        return balance -= withdrawalMoney;
+    }
+    //để gửi vào tài khoản một số tiền
+    public double depositMoney(double deposits) {
+        return balance += deposits;
+    }
+
 }
--- AccountTest.java
+++ AccountTest.java
@@ -1,6 +1,17 @@
+import java.util.Date;
+
 public class AccountTest {
 
     public static void main(String[] args) {
-        Account account = new Account();
+        Account account = new Account(1122,20.000,4.5);
+        account.withDraw(2.5000);
+        account.depositMoney(3.000);
+        account.setDateCreated(new Date());
+        double balance = account.getBalance();
+        System.out.println("Balance is:" +balance);
+        double monthInterest = account.getMonthlyInterest();
+        System.out.println("monthInterest"+monthInterest);
+        String dateCreated = account.getDateCreated().toString();
+        System.out.println("Date Created is: " + dateCreated);
     }
 }
